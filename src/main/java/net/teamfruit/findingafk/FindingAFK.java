efc4ac8ed3d79e221ea778f3d820e2020004be09
Kamesuta <kamesuta@gmail.com>
2020-10-08 02:49:28 +0900

    テストコマンド

--- src/main/java/net/teamfruit/findingafk/FindingAFK.java
+++ src/main/java/net/teamfruit/findingafk/FindingAFK.java
@@ -1,12 +1,13 @@
 package net.teamfruit.findingafk;
 
-import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
 import org.bukkit.Server;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
 import org.bukkit.plugin.java.JavaPlugin;
+
+import java.util.function.Consumer;
 
 public final class FindingAFK extends JavaPlugin implements CommandExecutor {
     @Override
@@ -24,10 +25,11 @@ public final class FindingAFK extends JavaPlugin implements CommandExecutor {
     public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
         Server server = getServer();
 
-        server.broadcastMessage(ChatColor.GREEN + "========= ▼リスポせず放置一覧▼ =========");
+        Consumer<String> chat = "finddeath".equals(command.getName()) ? server::broadcastMessage : sender::sendMessage;
+        chat.accept(ChatColor.GREEN + "========= ▼リスポせず放置してるバカ一覧▼ =========");
         server.getOnlinePlayers().stream().filter(e -> e.isDead()).forEach(e ->
-                Bukkit.broadcastMessage(ChatColor.DARK_GREEN + e.getName()));
-        server.broadcastMessage(ChatColor.GREEN + "========= ▲リスポせず放置一覧▲ =========");
+                chat.accept(ChatColor.DARK_GREEN + e.getName()));
+        chat.accept(ChatColor.GREEN + "========= ▲リスポせず放置してるバカ一覧▲ =========");
 
         return true;
     }
